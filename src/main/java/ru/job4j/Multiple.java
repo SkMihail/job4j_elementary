04a99bfe6413bfd63fa74471a1cd2c136d1b0ad6
SkMihail <msklyarov@gmail.com>
2023-01-06 10:28:25 +1000

    4.2. Заголовок в файле java [#159036] Заменён перенос строки "\n", на платформонезависимый System.lineSeparator();

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -3,7 +3,7 @@ package ru.job4j;
 public class Multiple {
     public static void main(String[] args) {
         for (int i = 1; i < 10; i++) {
-            System.out.printf("1 * %d = %d\n", i, 1 * i);
+            System.out.printf("1 * %d = %d%s", i, 1 * i, System.lineSeparator());
         }
     }
 }
