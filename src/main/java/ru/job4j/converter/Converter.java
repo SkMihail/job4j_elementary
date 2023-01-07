85c4eedc71dc15270c087cdf3b7f5804ede11549
SkMihail <msklyarov@gmail.com>
2023-01-07 18:08:19 +1000

    0. Что такое тестирование [#159431 #378193] Убрал округления через приведение.

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -4,23 +4,19 @@ public class Converter {
 
     public static float rubleToEuro(float value) {
         float rsl = value / 70;
-        int buf = (int) (rsl * 10000);
-        rsl = (float) buf / 10000;
         return rsl;
     }
 
     public static float rubleToDollar(float value) {
         float rsl = value / 60;
-        int buf = (int) (rsl * 10000);
-        rsl = (float) buf / 10000;
         return rsl;
     }
 
     public static void main(String[] args) {
         float euro = rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
-        float dollar = rubleToDollar(140);
-        System.out.println("140 rubles are " + dollar + " dollar.");
+        float dollar = rubleToDollar(120);
+        System.out.println("120 rubles are " + dollar + " dollar.");
 
         float inrubleToEuro = 140;
         float expected = 2;
@@ -28,8 +24,8 @@ public class Converter {
         boolean passedRubleToEuro = expected == outEuro;
         System.out.println("140 rubles are 2. Test result : " + passedRubleToEuro);
 
-        float inRubleToDollar = 140;
-        float expectedDollar = 2.3333F;
+        float inRubleToDollar = 120;
+        float expectedDollar = 2F;
         float outDollar = Converter.rubleToDollar(inRubleToDollar);
         boolean passedRubleToDollar = expectedDollar == outDollar;
         System.out.println("120 rubles are 2. Test result : " + passedRubleToDollar);
