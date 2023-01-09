16dfbebc8ea94eb69e940c99b737d4c670c7d839
SkMihail <msklyarov@gmail.com>
2023-01-09 20:52:09 +1000

    4.3.6. Math.abs [#373616 #379024] added method isValueOnBoard(int i)

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -9,15 +9,16 @@ public class ChessBoard {
         return rsl;
     }
 
+    public static boolean isValueOnBoard(int i) {
+        return i >= 0 && i <= 7;
+    }
+
     public static boolean coordinateIsValid(int x1, int y1, int x2, int y2) {
-        return x1 >= 0 && x1 <= 7
-                && x2 >= 0 && x2 <= 7
-                && y1 >= 0 && y1 <= 7
-                && y2 >= 0 && y2 <= 7;
+        return isValueOnBoard(x1) && isValueOnBoard(y1)
+                && isValueOnBoard(x2) && isValueOnBoard(y2);
     }
 
     public static boolean stepIsValid(int x1, int y1, int x2, int y2) {
        return Math.abs(x2 - x1) == Math.abs(y2 - y1);
     }
-
 }
