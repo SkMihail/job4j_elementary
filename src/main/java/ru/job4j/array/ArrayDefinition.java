acf62f752bb3318f5c4ed5683c7f8b5156b0af2f
SkMihail <msklyarov@gmail.com>
2023-01-11 22:00:45 +1000

    6.0.2. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,7 +3,10 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
+        System.out.printf("Размер массива ages = %d,%s", ages.length, System.lineSeparator());
         String[] surnames = new String[100500];
+        System.out.printf("Размер массива surnames = %d,%s", surnames.length, System.lineSeparator());
         float[] prices = new float[40];
+        System.out.printf("Размер массива prices = %d,%s", prices.length, System.lineSeparator());
     }
 }
