f7c27b1dd0c68c9d4efb3d573af7a77b46958955
SkMihail <msklyarov@gmail.com>
2023-01-12 17:10:52 +1000

    5.7. Ипотека [#156315 #379956]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,11 +4,11 @@ public class Mortgage {
 
     public static int year(double amount, double salary, double percent) {
         int year = 0;
-        double debtOnCredit = amount;
-        while (debtOnCredit > 0) {
-            debtOnCredit = debtOnCredit + debtOnCredit * percent / 100 - salary;
+        while (amount > 0) {
+            amount = amount + amount * percent / 100 - salary;
             year++;
         }
         return year;
     }
 }
+
